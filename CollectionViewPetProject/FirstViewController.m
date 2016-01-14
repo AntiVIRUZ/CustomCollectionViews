45c48116bbf92393938d817c170bc72d1b318472
Vasiliy Yatsevich <vy@mpk.mobi>
2016-01-14 19:53:08 +0300

    Project has been created

---
+++ CollectionViewPetProject/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  CollectionViewPetProject
+//
+//  Created by Vasiliy on 14.01.16.
+//  Copyright © 2016 MPK. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CollectionViewPetProject/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  CollectionViewPetProject
+//
+//  Created by Vasiliy on 14.01.16.
+//  Copyright © 2016 MPK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
